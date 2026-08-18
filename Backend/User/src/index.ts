7ad816c1f7a032a88897a7c5b4e6c090942e3787
RahulRajKhadka <sirrahul9848@gmail.com>
2026-08-18 15:26:47 +0545

    fix : server setup with Redis integration and error handling

--- Backend/User/src/index.ts
+++ Backend/User/src/index.ts
@@ -1,13 +1,42 @@
-import express from "express"
+import dotenv from "dotenv";
+import express from "express";
+import connectDb from "./config/db.js";
+import dns from "node:dns";
+import { createClient } from "redis";
+
+import userRoutes from "./routes/user.js"
+
+const PORT = process.env.PORT || 5000;
+const REDIS_URL = process.env.REDIS_URL;
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+dotenv.config();
+
+const app = express();
+
+app.use("api/v1",userRoutes)
 
 
-const app =express()
+if (!REDIS_URL) {
+  throw new Error("REDIS_URL is not defined in environment variables");
+}
 
-const port =500
+export const redisClient = createClient({
+  url: REDIS_URL,
+});
 
-app.listen(port, ()=>{
 
 
-    console.log(`server is running on port ${port}`);
+try {
+  await connectDb();
+  await redisClient.connect();
+  console.log("connected to redis");
 
-})
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+} catch (err) {
+  console.error("Startup failed:", err);
+  process.exit(1);
+}
