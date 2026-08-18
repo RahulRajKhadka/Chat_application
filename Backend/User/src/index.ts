049a64f726232f45951ce4ec9d9610ff9e9e20e0
RahulRajKhadka <sirrahul9848@gmail.com>
2026-08-18 15:32:42 +0545

    feat: add amqplib and its type definitions

--- Backend/User/src/index.ts
+++ Backend/User/src/index.ts
@@ -4,19 +4,18 @@ import connectDb from "./config/db.js";
 import dns from "node:dns";
 import { createClient } from "redis";
 
-import userRoutes from "./routes/user.js"
+import userRoutes from "./routes/user.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const REDIS_URL = process.env.REDIS_URL;
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-dotenv.config();
-
 const app = express();
 
-app.use("api/v1",userRoutes)
-
+app.use("/api/v1", userRoutes);
 
 if (!REDIS_URL) {
   throw new Error("REDIS_URL is not defined in environment variables");
@@ -26,11 +25,10 @@ export const redisClient = createClient({
   url: REDIS_URL,
 });
 
-
-
 try {
   await connectDb();
   await redisClient.connect();
+
   console.log("connected to redis");
 
   app.listen(PORT, () => {
