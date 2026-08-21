830c330b2aa79b2ab54b051aa1ae8f74a4b825a3
RahulRajKhadka <sirrahul9848@gmail.com>
2026-08-21 21:05:41 +0545

    feat: add TryCatch middleware for error handling in Express

---
+++ Backend/User/src/config/TryCatch.ts
@@ -0,0 +1,25 @@
+import type {
+  NextFunction,
+  Request,
+  Response,
+  RequestHandler,
+} from "express";
+
+const TryCatch = (handler: RequestHandler): RequestHandler => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      await handler(req, res, next);
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+};
+
+export default TryCatch;
