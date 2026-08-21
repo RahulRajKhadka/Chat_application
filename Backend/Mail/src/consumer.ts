147f8e983178d57aad6922bd187c278a56abcd41
RahulRajKhadka <sirrahul9848@gmail.com>
2026-08-21 21:05:26 +0545

    fix(consumer): implement OTP email sending functionality using nodemailer

--- Backend/Mail/src/consumer.ts
+++ Backend/Mail/src/consumer.ts
@@ -26,6 +26,40 @@ export const startSendOtpConsumer = async () => {
 
     channel = await connection.createChannel();
 
+    const queueName = "send-otp";
+
+    await channel.assertQueue(queueName, { durable: true });
+    console.log("Mail service consumer started , listening for otp emails");
+
+    channel.consume(queueName, async (msg) => {
+      if (msg) {
+        try {
+          const { to, subject, body } = JSON.parse(msg.content.toString());
+
+          const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            auth: {
+              user: process.env.USER,
+              pass: process.env.PASSWORD,
+            },
+          });
+
+          await transporter.sendMail({
+            from: "Chat_App",
+            to,
+            subject,
+            text: body,
+          });
+
+          console.log(`OTP mail sent to ${to}`);
+          channel.ack(msg);
+        } catch (error) {
+          console.log("Failed to send the otp ", error);
+        }
+      }
+    });
+
     console.log("RabbitMQ consumer connected");
   } catch (error) {
     console.log("Failed to start rabbitmq consumer", error);
