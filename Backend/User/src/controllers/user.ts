16ec8c4c1a9fa5cab95fe9df024cbecc8b67ec35
RahulRajKhadka <sirrahul9848@gmail.com>
2026-08-24 23:07:25 +0545

    feat: implement OTP generation and email sending in loginUser function

---
+++ Backend/User/src/controllers/user.ts
@@ -0,0 +1,36 @@
+import { publishToQueue } from "../config/rabbitmq.js";
+import TryCatch from "../config/TryCatch.js";
+import { redisClient } from "../index.js";
+
+export const loginUser= TryCatch(async(req,res)=>{
+    const {email}=req.body
+
+    const RateLimitKey=`otp:ratelimit:${email}`
+    const rateLimit=await redisClient.get(RateLimitKey)
+    if(rateLimit){
+        res.status(429).json({
+            message:"Too many requests.please wait before requestin new otp",
+        })
+    }
+
+    const otp=Math.floor(100000 +Math.random()*900000).toString()
+    const otpkey=`otp:${email}`;
+    await redisClient.set(otpkey,otp,{
+        EX:300,
+    })
+
+    await redisClient.set(RateLimitKey,"true",{
+    EX:60,
+    });
+
+    const message={
+        to:email,
+        subject:"Your otp code",
+        body:`Your OTP is ${otp}. It is valid for 5 minutes`
+    }
+
+    await publishToQueue("send-otp",message)
+    res.status(429).json({
+        message:"OTP sent to your mail"
+    })
+})
