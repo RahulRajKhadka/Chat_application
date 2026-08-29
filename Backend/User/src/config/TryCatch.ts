c4ab52ef9d780ba6e6048de7e29858155462a7a9
RahulRajKhadka <sirrahul9848@gmail.com>
2026-08-29 21:25:43 +0545

    refactor: enhance TryCatch middleware to support generic request types

--- Backend/User/src/config/TryCatch.ts
+++ Backend/User/src/config/TryCatch.ts
@@ -1,25 +1,31 @@
 import type {
-  NextFunction,
-  Request,
-  Response,
-  RequestHandler,
+    NextFunction,
+    Request,
+    Response,
+    RequestHandler,
 } from "express";
 
-const TryCatch = (handler: RequestHandler): RequestHandler => {
-  return async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      await handler(req, res, next);
-    } catch (error: any) {
-      res.status(500).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  };
+const TryCatch = <T extends Request>(
+    handler: (
+        req: T,
+        res: Response,
+        next: NextFunction
+    ) => Promise<void>
+): RequestHandler => {
+    return async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> => {
+        try {
+            await handler(req as T, res, next);
+        } catch (error: any) {
+            res.status(500).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    };
 };
 
 export default TryCatch;
